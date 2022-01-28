8166277767915b6d2a10bd14f358e2fdd72d1955
Leonardo Gonçalves <leonardo@lginfo.com.br>
2022-01-28 15:56:31 -0300

    first commit

---
+++ js/principal.js
@@ -0,0 +1,10 @@
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista";
+
+var teste = document.getElementById("paciente-1");
+var testeTexto = teste.textContent;
+var paciente = document.querySelector("#paciente-1");
+var tdPeso = paciente.querySelector(".info-peso");
+//var valorPeso = tdPeso.textContent;
+
+alert(testeTexto);
