63be075eadb1f9c7add43473195f61f44cfc7309
Leonardo Gonçalves <leonardo@lginfo.com.br>
2022-02-04 17:39:13 -0300

    form add pacientes ok faltando IMC

--- js/principal.js
+++ js/principal.js
@@ -39,7 +39,36 @@ for (var i = 0; i < pacientes.length; i++) {
 
 var botaoAdicionar = document.querySelector("#adicionar-paciente");
 botaoAdicionar.addEventListener("click", (event) => {
-        event.preventDefault();
-        console.log("Oi eu sou o botao e fui clicado");
-    }
-);
+    event.preventDefault();
+    console.log("Oi eu sou o botao e fui clicado");
+
+    var formAdd = document.querySelector("#form-adiciona");
+
+    var nomeAdd = formAdd.nome.value;
+    var pesoAdd = formAdd.peso.value;
+    var alturaAdd = formAdd.altura.value;
+    var gorduraAdd = formAdd.gordura.value;
+
+    var pacienteLinha = document.createElement("tr");
+    
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+
+    nomeTd.textContent = nomeAdd.toUpperCase();
+    pesoTd.textContent = pesoAdd;
+    alturaTd.textContent = alturaAdd;
+    gorduraTd.textContent = gorduraAdd;
+
+    pacienteLinha.appendChild(nomeTd);
+    pacienteLinha.appendChild(pesoTd);
+    pacienteLinha.appendChild(alturaTd);
+    pacienteLinha.appendChild(gorduraTd);
+
+    var tabelaAdd = document.querySelector("#tabela-pacientes");
+    tabelaAdd.appendChild(pacienteLinha);
+
+
+});
