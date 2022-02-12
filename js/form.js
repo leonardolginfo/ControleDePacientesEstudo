6878ffb5df07912ca0dd0b6f8d2f3268f9a61309
Leonardo Gonçalves <leonardo@lginfo.com.br>
2022-02-12 18:24:17 -0300

    refatorando js

--- js/form.js
+++ js/form.js
@@ -1,40 +1,68 @@
 var botaoAdicionar = document.querySelector("#adicionar-paciente");
 botaoAdicionar.addEventListener("click", (event) => {
-            event.preventDefault();
+    event.preventDefault();
 
-            var formAdd = document.querySelector("#form-adiciona");
-            if (formAdd.nome.value == "" || formAdd.peso.value == "" ||
-                formAdd.altura.value == "" || formAdd.gordura.value == "") {
-                alert("Formulário possui um ou mais campos em branco.")
-            } else {
-                var nomeAdd = formAdd.nome.value;
-                var pesoAdd = formAdd.peso.value;
-                var alturaAdd = formAdd.altura.value;
-                var gorduraAdd = formAdd.gordura.value;
+    var formAdd = document.querySelector("#form-adiciona");
 
-                var pacienteLinha = document.createElement("tr");
+    if (formAdd.nome.value == "" || formAdd.peso.value == "" ||
+        formAdd.altura.value == "" || formAdd.gordura.value == "") {
+        alert("Formulário possui um ou mais campos em branco.")
+    } else {
 
-                var nomeTd = document.createElement("td");
-                var pesoTd = document.createElement("td");
-                var alturaTd = document.createElement("td");
-                var gorduraTd = document.createElement("td");
-                var imcTd = document.createElement("td");
+        //extrair dados do formAdd
+        var paciente = pegarDadosFormAdd(formAdd);
+        console.log(paciente);
 
-                nomeTd.textContent = nomeAdd;
-                pesoTd.textContent = pesoAdd;
-                alturaTd.textContent = alturaAdd;
-                gorduraTd.textContent = gorduraAdd;
-                imcTd.textContent = calculaImc(peso, altura);
+        //montar tr com paciente
+        var pacienteLinha = montaTr(paciente)
 
-                pacienteLinha.appendChild(nomeTd);
-                pacienteLinha.appendChild(pesoTd);
-                pacienteLinha.appendChild(alturaTd);
-                pacienteLinha.appendChild(gorduraTd);
-                pacienteLinha.appendChild(imcTd);
+        var tabelaAdd = document.querySelector("#tabela-pacientes");
+        tabelaAdd.appendChild(pacienteLinha);
+    }
 
-                var tabelaAdd = document.querySelector("#tabela-pacientes");
-                tabelaAdd.appendChild(pacienteLinha);
+    function pegarDadosFormAdd(form) {
 
+        var paciente = {
+            nome: formAdd.nome.value,
+            altura: formAdd.altura.value,
+            peso: formAdd.peso.value,
+            gordura: formAdd.gordura.value,
+            imc: calculaImc(formAdd.peso.value, formAdd.altura.value),
+        }
+        return paciente;
+    }
 
-            }
-        });
+    function montaTr(paciente) {
+
+        var pacienteLinha = document.createElement("tr");
+        pacienteLinha.classList.add("paciente");
+
+        var nomeTd = montaTd(paciente.nome, "info-nome");
+        var pesoTd = montaTd(paciente.peso, "info-peso");
+        var alturaTd = montaTd(paciente.altura, "info-altura");
+        var gorduraTd = montaTd(paciente.gordura, "info-gordura");
+        var imcTd = montaTd(paciente.imc, "info-imc");
+
+        nomeTd.textContent = paciente.nome;
+        pesoTd.textContent = paciente.peso;
+        alturaTd.textContent = paciente.altura;
+        gorduraTd.textContent = paciente.gordura;
+        imcTd.textContent = paciente.imc;
+
+        pacienteLinha.appendChild(nomeTd);
+        pacienteLinha.appendChild(pesoTd);
+        pacienteLinha.appendChild(alturaTd);
+        pacienteLinha.appendChild(gorduraTd);
+        pacienteLinha.appendChild(imcTd);
+
+        return pacienteLinha;
+
+    }
+
+    function montaTd(dado, classe) {
+        var td = document.createElement("td");
+        td.textContent = dado;
+        td.classList.add(classe);
+        return td
+    }
+});
